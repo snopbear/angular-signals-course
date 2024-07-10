bad2bc6ecc5993153c6a12ab12a4507438b5eae8
snopbear <snopbear@gmail>
2024-07-10 15:43:49 +0200

    interceptors

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -3,14 +3,14 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {LoadingService} from "./loading.service";
 
 @Component({
-  selector: "loading",
-  templateUrl: "./loading.component.html",
-  styleUrls: ["./loading.component.scss"],
+  selector: 'loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss'],
   imports: [MatProgressSpinner],
   standalone: true,
 })
 export class LoadingIndicatorComponent {
+  loadingService = inject(LoadingService);
 
-
-
+  loading:Signal<boolean> = this.loadingService.loading;
 }
