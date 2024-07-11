6ee886cd2bdfaf35295e4d5162bc4c5e1f638061
snopbear <snopbear@gmail>
2024-07-11 19:40:10 +0200

    update-lessons

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, input, output} from '@angular/core';
+import {Component, ElementRef, effect, inject, input, output, viewChildren} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {Course} from "../models/course.model";
 import {MatDialog} from "@angular/material/dialog";
@@ -19,6 +19,14 @@ export class CoursesCardListComponent {
   courseDeleted = output<string>();
 
   dialog = inject(MatDialog);
+
+  courseCards = viewChildren<ElementRef>('courseCard');
+
+constructor(){
+effect(()=>{
+  
+});
+}
 
   async onEditCourse(course: Course) {
     const newCourse = await openEditCourseDialog(this.dialog, {
